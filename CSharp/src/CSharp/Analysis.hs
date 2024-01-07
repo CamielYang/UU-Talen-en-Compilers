f19c5a35e668f3601f47245703008a43d3d92fc7
CamielYang <yangcamiel@gmail.com>
2024-01-07 12:20:00 +0100

    Code cleanup + alignments

--- CSharp/src/CSharp/Analysis.hs
+++ CSharp/src/CSharp/Analysis.hs
@@ -31,7 +31,7 @@ data Env = Env {
 } deriving (Show)
 
 baseERet = ERet {
-  eIdent = "",
+  eIdent   = "",
   eRetType = TyVoid
 }
 
@@ -60,8 +60,8 @@ analysisAlgebra = CSharpAlgebra
 emptyEnv :: Env
 emptyEnv = Env {
   compilation = Syntax,
-  denv = M.empty,
-  menv = M.empty
+  denv        = M.empty,
+  menv        = M.empty
 }
 
 insertDecl :: Decl -> Env -> Env
@@ -70,7 +70,7 @@ insertDecl (Decl rt i) env = env { denv = M.insert i rt (denv env) }
 getDecl :: Ident -> Env -> RetType
 getDecl i Env{..}
   | M.member i denv = denv M.! i
-  | otherwise = error ("TypeError: " ++ show i ++ " is not defined")
+  | otherwise       = error ("TypeError: " ++ show i ++ " is not defined")
 
 insertMeth :: RetType -> [Decl] -> Ident -> Env -> Env
 insertMeth rt ds i env = env { menv = M.insert i (Method rt ds) (menv env) }
@@ -78,22 +78,22 @@ insertMeth rt ds i env = env { menv = M.insert i (Method rt ds) (menv env) }
 getMeth :: Ident -> Env -> Method
 getMeth i Env{..}
   | M.member i menv = menv M.! i
-  | otherwise = error ("TypeError: " ++ show i ++ " is not defined")
+  | otherwise       = error ("TypeError: " ++ show i ++ " is not defined")
 
 fClass :: ClassName -> [M] -> C
-fClass _ ms = trace (show passSyntax ++ show passAnalysis)
-  passSyntax `seq`
+fClass _ ms =
+  passSyntax   `seq`
   passAnalysis `seq`
   True
   where
-    passSyntax = foldl f (emptyEnv { compilation = Syntax }) ms
+    passSyntax   = foldl f (emptyEnv   { compilation = Syntax   }) ms
     passAnalysis = foldl f (passSyntax { compilation = Analysis }) ms
-    f env m = let env' = m env in env'
+    f env m      = m env
 
 fMembDecl :: Decl -> M
 fMembDecl d env@Env{..}
   | compilation == Syntax = insertDecl d env
-  | otherwise = env
+  | otherwise             = env
 
 fMembExpr :: E -> M
 fMembExpr e env = e env `seq` env
@@ -101,24 +101,35 @@ fMembExpr e env = e env `seq` env
 fMembMeth :: RetType -> Ident -> [Decl] -> S -> M
 fMembMeth rt i ps s env@Env{..}
   | compilation == Syntax = s (foldr insertDecl (insertMeth rt ps i env) ps)
-  | otherwise = s env
+  | otherwise             = s env
 
 fStatDecl :: Decl -> S
 fStatDecl d env@Env{..}
   | compilation == Syntax = insertDecl d env
-  | otherwise = env
+  | otherwise             = env
 
 fStatExpr :: E -> S
-fStatExpr e  env@Env{..} = e env `seq` env
+fStatExpr e env@Env{..} =
+  e env `seq`
+  env
 
 fStatIf :: E -> S -> S -> S
-fStatIf e s1 s2 env@Env{..} = e env `seq` s1 env `seq` s2 env `seq` env
+fStatIf e s1 s2 env@Env{..} =
+  e  env `seq`
+  s1 env `seq`
+  s2 env `seq`
+  env
 
 fStatWhile :: E -> S -> S
-fStatWhile e s env@Env{..} = e env `seq` s env `seq` env
+fStatWhile e s env@Env{..} =
+  e env `seq`
+  s env `seq`
+  env
 
 fStatReturn :: E -> S
-fStatReturn e env@Env{..} = e env `seq` env
+fStatReturn e env@Env{..} =
+  e env `seq`
+  env
 
 fStatBlock :: [S] -> S
 fStatBlock ss env@Env{..} = foldl (\env s -> s env) env ss
@@ -132,7 +143,7 @@ fExprLit l env = baseERet { eRetType = n }
 
 fExprVar :: Ident -> E
 fExprVar i env = getDecl i env `seq` baseERet {
-  eIdent = i,
+  eIdent   = i,
   eRetType = getDecl i env
 }
 
@@ -140,39 +151,44 @@ variableError :: Ident -> RetType -> RetType -> Env -> a
 variableError v r1 r2 env = error ("TypeError: \"" ++ v ++ "\" expected (" ++ show r1 ++ ") but got (" ++ show r2 ++ ")")
 
 fExprOp :: Operator -> E -> E -> E
-fExprOp _ _ _ e@Env { compilation = Syntax } = baseERet
+fExprOp _ _ _ Env { compilation = Syntax } = baseERet
 fExprOp OpAsg address value env
-  | getDecl (eIdent $ address env) env == eRetType (value env) = baseERet
-  | otherwise = variableError (eIdent $ address env) (getDecl (eIdent $ address env) env) (eRetType (value env)) env
+  | addRt == valRt = baseERet
+  | otherwise      = variableError addId addRt valRt env
+  where
+    addId = eIdent $ address env
+    addRt = getDecl addId env
+    valRt = eRetType (value env)
 fExprOp op e1 e2 env =
-  e1 env `seq` e2 env `seq` baseERet {
-    eRetType = case op of
-      {
-        OpAdd -> NV TyInt; OpSub -> NV TyInt; OpMul -> NV TyInt; OpDiv -> NV TyInt;
-      ; OpMod -> NV TyInt
-      ; OpXor -> NV TyBool; OpAnd -> NV TyBool; OpOr  -> NV TyBool;
-      ; OpLeq -> NV TyBool; OpLt -> NV TyBool;
-      ; OpGeq -> NV TyBool; OpGt -> NV TyBool;
-      ; OpEq  -> NV TyBool; OpNeq -> NV TyBool;
-      }
+  e1 env `seq`
+  e2 env `seq`
+  baseERet {
+    eRetType = case op of {
+        OpMul -> NV TyInt ;   OpDiv -> NV TyInt ;   OpMod -> NV TyInt ;
+        OpAdd -> NV TyInt ;   OpSub -> NV TyInt ;
+        OpLeq -> NV TyBool;   OpLt  -> NV TyBool;   OpGeq -> NV TyBool;   OpGt  -> NV TyBool;
+        OpEq  -> NV TyBool;   OpNeq -> NV TyBool;
+        OpXor -> NV TyBool;
+        OpAnd -> NV TyBool;
+        OpOr  -> NV TyBool; }
   }
 
 fExprCall :: Ident -> [E] -> E
 fExprCall _ _ e@Env { compilation = Syntax } = baseERet
 fExprCall "print" es env
-  | all (\e -> eRetType (e env) /= TyVoid) es = foldr f baseERet es
-  | otherwise = error "TypeError: print cannot have void arguments"
+  | checkPrint = foldr f baseERet es
+  | otherwise  = error "TypeError: print cannot have void arguments"
   where
     f e baseERet = e env `seq` baseERet
-    checkPrint = all (\e -> eRetType (e env) /= TyVoid) es
+    checkPrint   = all (\e -> eRetType (e env) /= TyVoid) es
 fExprCall i es env
   | checkMethod = foldr f (baseERet { eRetType = mRetType }) es
-  | otherwise = error ("TypeError: " ++ show i ++ " expected " ++ show (length mParams) ++ " arguments, but got " ++ show (length es))
+  | otherwise   = error ("TypeError: " ++ show i ++ " expected " ++ show (length mParams) ++ " arguments, but got " ++ show (length es))
   where
-    Method{..} = getMeth i env
-    f e baseERet = e env `seq` baseERet
-    checkMethod = length es == length mParams && checkArgs mParams es
+    Method{..}      = getMeth i env
+    f e baseERet    = e env `seq` baseERet
+    checkMethod     = length es == length mParams && checkArgs mParams es
     checkArgs [] [] = True
     checkArgs (Decl d di:ds) (e:es)
       | eRetType (e env) == d = checkArgs ds es
-      | otherwise = variableError (i ++ " arg " ++ di) d (eRetType (e env)) env
+      | otherwise             = variableError (i ++ " arg " ++ di) d (eRetType (e env)) env
